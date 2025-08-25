eb3ea44de29353bdde04cba3b96383b6062564ae
DanGarcia963 <de.espejelg@gmail.com>
2025-08-24 18:39:04 -0600

    Conection DB

--- helpers/connectionMySQL.js
+++ helpers/connectionMySQL.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise'
+import 'dotenv/config'
 
-const DEFAULT_CONFIG = { host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: '',
-  database: 'f1'
+const connection = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 }
-const connection = process.env.DATABASE_URL ?? DEFAULT_CONFIG
 
 export const connectionMySQL = await mysql.createConnection(connection)
